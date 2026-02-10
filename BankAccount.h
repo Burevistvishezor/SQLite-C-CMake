75fe8deba8d401adc664a123d8b11bb75c1a5354
Banking simulations <ladimp11@gmail.com>
2026-02-10 16:43:53 +0300

    Update BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -1,1 +1,21 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
 
+#include <string>
+
+class BankAccount {
+private:
+    std::string owner;
+    double balance;
+    double interestRate = 0.02; // 2% annual interest
+
+public:
+    BankAccount(const std::string& ownerName, double initialBalance);
+    void deposit(double amount);
+    void withdraw(double amount);
+    void applyInterest();
+    double getBalance() const;
+    std::string getOwner() const;
+};
+
+#endif // BANKACCOUNT_H
