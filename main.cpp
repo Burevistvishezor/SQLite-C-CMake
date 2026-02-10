64c8337fe17aa5f4af0e2cef127f2e7146f3ee32
Banking simulations <ladimp11@gmail.com>
2026-02-10 16:42:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "BankAccount.h"
+
+int main() {
+    BankAccount account("Vince", 1000.0);
+
+    std::cout << "Initial balance: " << account.getBalance() << " EUR\n";
+
+    account.deposit(500.0);
+    std::cout << "After deposit of 500 EUR: " << account.getBalance() << " EUR\n";
+
+    account.withdraw(200.0);
+    std::cout << "After withdrawal of 200 EUR: " << account.getBalance() << " EUR\n";
+
+    account.applyInterest();
+    std::cout << "After applying interest: " << account.getBalance() << " EUR\n";
+
+    return 0;
+}
 
