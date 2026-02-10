182f19e07bc2c158fdc232afb9f9597da9ea9108
Banking simulations <ladimp11@gmail.com>
2026-02-10 16:45:38 +0300

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,1 +1,27 @@
+#include "BankAccount.h"
+#include <iostream>
+
+BankAccount::BankAccount(const std::string& ownerName, double initialBalance)
+    : owner(ownerName), balance(initialBalance) {}
+
+void BankAccount::deposit(double amount) {
+    if (amount > 0) balance += amount;
+}
+
+void BankAccount::withdraw(double amount) {
+    if (amount > 0 && amount <= balance) balance -= amount;
+    else std::cout << "Insufficient funds\n";
+}
+
+void BankAccount::applyInterest() {
+    balance += balance * interestRate;
+}
+
+double BankAccount::getBalance() const {
+    return balance;
+}
+
+std::string BankAccount::getOwner() const {
+    return owner;
+}
 
