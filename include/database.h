71af00a5ffcf851f776fc8bafaf853577c23400a
Banking simulations <ladimp11@gmail.com>
2026-06-01 22:16:20 +0300

    Update database.h

--- include/database.h
+++ include/database.h
@@ -5,3 +5,24 @@ cd /путь/к/вашему/проекту  # перейдите в папку 
 mkdir include
 mkdir src
 mkdir tests   # если не нужен, пропустите
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <sqlite3.h>
+#include <string>
+
+class Database {
+public:
+    explicit Database(const std::string& db_name);
+    ~Database();
+
+    bool open();
+    void close();
+    bool execute(const std::string& sql);
+
+private:
+    std::string db_name_;
+    sqlite3* db_;
+};
+
+#endif // DATABASE_H
