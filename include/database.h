e4c8203c4b86a2b315e12e2ad9a739cd87a11497
Banking simulations <ladimp11@gmail.com>
2026-06-01 22:14:32 +0300

    Update database.h

--- include/database.h
+++ include/database.h
@@ -1,3 +1,7 @@
 mkdir -p SQLite-C-CMake/include
 mkdir -p SQLite-C-CMake/src
 mkdir -p SQLite-C-CMake/tests
+cd /путь/к/вашему/проекту  # перейдите в папку проекта
+mkdir include
+mkdir src
+mkdir tests   # если не нужен, пропустите
